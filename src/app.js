d84668c836d7a64879c4abe14b578e448c6e9811
AlonaAloha <alona.iakovlieva@gmail.com>
2022-09-23 18:32:58 +0100

    Display forecast data using the API

--- src/app.js
+++ src/app.js
@@ -21,24 +21,38 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
   }
 
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    return days[day];
+
+  }
+
   function displayForecast(response) {
+   let forecast = response.data.daily;
    let forecastElement = document.querySelector("#forecast");
    
    let forecastHTML = `<div class="row">`;
-   let days = ["Fri","Sat","Sun","Mon","Tue","Wed","Thu"];
-   days.forEach(function(day){
+   
+   forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
     forecastHTML = forecastHTML +
             `<div class="col-2">
               <div class="weather-forecast-date">
-                ${day}
+                ${formatDay(forecastDay.dt)}
               </div>
-              <img src="	http://openweathermap.org/img/wn/01n@2x.png" alt="" width="36" />
+              <img src="	http://openweathermap.org/img/wn/
+              ${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
               <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">12°</span>
-                <span class="weather-forecast-temperature-min">/8°</span>
+                <span class="weather-forecast-temperature-max">
+                ${Math.round(forecastDay.temp.max)}°</span>
+                <span class="weather-forecast-temperature-min">/
+                 ${Math.round(forecastDay.temp.min)}°</span>
               </div>
-          </div>`; 
-   })
+          </div>`;
+          } 
+   });
 
     forecastHTML = forecastHTML + `</div>`;
    forecastElement.innerHTML = forecastHTML;
@@ -46,7 +60,7 @@ function formatDate(timestamp) {
   }
 function getForecast(coordinates) {
 let apiKey ="854d931b03dfa3d9b694dd7d5ecb6684";
-let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
 lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
@@ -72,7 +86,7 @@ celsiusTemperature = response.data.main.temp;
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
   iconElement.setAttribute("alt",response.data.weather[0].description);
 
-  getForecast(response.date.coord);
+  getForecast(response.data.coord);
 }
 
 
