86bdfb069f4affe9579c6b8b723adf8b7ed28b54
AlonaAloha <alona.iakovlieva@gmail.com>
2022-09-23 12:31:54 +0100

    Added forecast from HTML to JS

--- src/app.js
+++ src/app.js
@@ -20,6 +20,31 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
   }
+
+  function displayForecast() {
+   let forecastElement = document.querySelector("#forecast");
+   
+   let forecastHTML = `<div class="row">`;
+   let days = ["Fri","Sat","Sun","Mon","Tue","Wed"];
+   days.forEach(function(day){
+    forecastHTML = forecastHTML +
+            `<div class="col-2">
+              <div class="weather-forecast-date">
+                ${day}
+              </div>
+              <img src="	http://openweathermap.org/img/wn/01n@2x.png" alt="" width="36" />
+              <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">12°</span>
+                <span class="weather-forecast-temperature-min">/8°</span>
+              </div>
+          </div>`; 
+   })
+
+    forecastHTML = forecastHTML + `</div>`;
+   forecastElement.innerHTML = forecastHTML;
+   
+  }
+
 function displayTemperature(response) {
   
   let temperatureElement = document.querySelector("#temperature");
@@ -89,3 +114,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click",displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
